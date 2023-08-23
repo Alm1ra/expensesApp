5fa8c25e223079f2019392c5ce864b52fae4dc1d
Alm1ra <frydlewicz.ola@gmail.com>
2023-08-23 15:06:36 +0200

    Added a wrapper component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,5 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
 import './ExpenseList.css'
+import Card from "./Card";
 
 function ExpenseList(props) {
     const expenseArr = [];
@@ -7,6 +8,6 @@ function ExpenseList(props) {
     for(let i = 0; i < props.expense.length; i++) {
         expenseArr.push(<ExpenseItem key={props.expense[i].id} title={props.expense[i].title} amount={props.expense[i].amount} date={props.expense[i].date}/>)
     }
-    return <div className="expenses">{expenseArr}</div>
+    return <Card className="expenses">{expenseArr}</Card>
 }
 export default ExpenseList;
